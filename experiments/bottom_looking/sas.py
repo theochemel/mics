fix range, add quadratic scaling

experiments/bottom_looking/sas.py
@@ -4,9 +4,15 @@ import torch
 from config import Config
 
 
-def get_sas_updates(points: np.array, sinks: np.array, signal_t: np.array, signals: np.array, config: Config) -> np.array:
+def get_sas_updates(points: np.array,
+                    sinks: np.array,
+                    source: np.array,
+                    signal_t: np.array,
+                    signals: np.array,
+                    config: Config) -> np.array:
     # points is [n, 3]
     # sinks is [s, 3]
+    # source is [3]
     # signal_t is [d]
     # signals is [s, d]
 
@@ -15,6 +21,7 @@ def get_sas_updates(points: np.array, sinks: np.array, signal_t: np.array, signa
     with torch.no_grad():
         points = torch.tensor(points).to(device)
         sinks = torch.tensor(sinks).to(device)
+        source = torch.tensor(source).to(device)
         signal_t = torch.tensor(signal_t).to(device)
         signals = torch.tensor(signals, dtype=torch.complex128).to(device)
 
@@ -23,15 +30,18 @@ def get_sas_updates(points: np.array, sinks: np.array, signal_t: np.array, signa
         d = len(signal_t)
 
         # [n, s, 3]
-        delta = points.unsqueeze(1) - sinks.unsqueeze(0)
+        delta_sinks = points.unsqueeze(1) - sinks.unsqueeze(0)
+
+        # [n, 3]
+        delta_source = points - source.unsqueeze(0)
 
         # [n, s]
-        r = torch.sqrt(delta[:, :, 0] ** 2 + delta[:, :, 1] ** 2)
-        angle = torch.arctan2(r, -delta[:, :, 2])
+        # r = torch.sqrt(delta[:, :, 0] ** 2 + delta[:, :, 1] ** 2)
+        # angle = torch.arctan2(r, -delta[:, :, 2])
 
         # [n, s]
-        range = torch.linalg.norm(delta, axis=-1)
-        rtt = (2 * range) / config.c
+        range = torch.linalg.norm(delta_sinks, axis=-1) + torch.linalg.norm(delta_source, axis=-1).unsqueeze(1)
+        rtt = range / config.c
 
         # [n, s]
         d_interp = (rtt - signal_t[0]) / config.Ts
@@ -40,7 +50,7 @@ def get_sas_updates(points: np.array, sinks: np.array, signal_t: np.array, signa
         d_a = d_interp - d_i
 
         # [n, s]
-        s_i = torch.arange(s).unsqueeze(0).repeat((n, 1))
+        s_i = torch.arange(s, device=device).unsqueeze(0).repeat((n, 1))
 
         # [n, s]
         valid = (0 <= d_i) & (d_i_plus_1 < d)#  & (torch.abs(angle) < config.fov / 2)
@@ -49,10 +59,11 @@ def get_sas_updates(points: np.array, sinks: np.array, signal_t: np.array, signa
         interp_pulse = (1 - d_a[valid]) * signals[s_i[valid], d_i[valid]] + d_a[valid] * signals[s_i[valid], d_i_plus_1[valid]]
 
         # [n, s]
-        updates = torch.zeros((n, s), dtype=torch.complex128)
+        updates = torch.zeros((n, s), dtype=torch.complex128, device=device)
 
         # [n, s]
         updates[valid] = interp_pulse * torch.exp(1.0j * np.pi * config.chirp_fc * rtt[valid])
+        updates *= range ** 2
 
         # [s, n]
         updates = torch.transpose(updates, dim0=0, dim1=1)