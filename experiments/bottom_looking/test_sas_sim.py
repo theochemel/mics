fix range, add quadratic scaling

experiments/bottom_looking/test_sas_sim.py
@@ -18,13 +18,15 @@ from visualize import plot_map_slices_animated
 def main():
     config = Config()
 
-    with open("no-cubes.pkl", "rb") as f:
+    with open("cubes-only.pkl", "rb") as f:
         exp = pickle.load(f)
 
     traj = exp["trajectory"]
     tx_pattern = exp["tx_pattern"]
     rx_pattern = exp["rx_pattern"]
     array = exp["array"]
+
+    source = np.array([0,0,0])
 
     grid_x, grid_y, grid_z = get_grid_points(config)
     grid_xy_extent = get_grid_xy_extent(config)
@@ -37,9 +39,10 @@ def main():
 
         pose = traj.poses[pose_i]
 
-        current_array_positions = (array.positions + pose.t)
+        current_array_positions = (array.positions + pose.t)#[4:5]
+        current_source_position = source + pose.t
 
-        raw_signals = rx_pattern[i]
+        raw_signals = rx_pattern[i]#[4:5]
         signal_t = config.Ts * np.arange(raw_signals.shape[-1]) - config.chirp_duration / 2
 
         signals = demod_signal(signal_t, raw_signals, config)
@@ -55,11 +58,6 @@ def main():
         # plt.show()
 
         # demod_tx = demod_signal(config.Ts * np.arange(len(tx_pattern.baseband)), tx_pattern.baseband, config)
-        #
-        # plt.plot(np.real(demod_tx[0]))
-        # plt.plot(np.imag(demod_tx[0]))
-        # plt.show()
-        #
         # pulse_tx = pulse_compress_signals(demod_tx, config)
         #
         # plt.plot(np.real(pulse_tx[0]))
@@ -72,22 +70,21 @@ def main():
 
         pulses = pulse_compress_signals(signals, config)
 
-        # plt.plot(config.c * signal_t / 2, np.real(pulses[0]))
-        # plt.plot(config.c * signal_t / 2, np.imag(pulses[0]))
-        # plt.show()
+        updates = get_sas_updates(grid_points, current_array_positions, current_source_position, signal_t, pulses, config)
 
-        updates = get_sas_updates(grid_points, current_array_positions, signal_t, pulses, config)
         updates = updates.reshape((updates.shape[0], grid_x.shape[0], grid_x.shape[1], grid_x.shape[2]))
 
         sum_updates = np.sum(updates, axis=0)
 
         map += sum_updates
 
-        plt.imshow(np.abs(map[:, :, 0]), extent=grid_xy_extent)
-    # plt.scatter(current_array_positions[:, 0], current_array_positions[:, 1])
-        plt.show()
 
-    # plot_map_slices_animated(map, grid_xy_extent)
+    plt.imshow(np.abs(map[:, :, 0]), extent=grid_xy_extent)
+    plt.imshow(np.log(np.abs(map[:, :, 0])), cmap='viridis', aspect='equal')
+    # plt.scatter(current_array_positions[:, 0], current_array_positions[:, 1])
+    plt.show()
+
+    plot_map_slices_animated(map, grid_xy_extent)
 
 
 if __name__ == "__main__":

experiments/cubes_with_seafloor.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         )
     ]
 
-    arr = RectangularArray(6, 6, 1e-2, UniformContinuousAngularDistribution(
+    arr = RectangularArray(3, 3, 1e-2, UniformContinuousAngularDistribution(
         min_az=-pi, max_az=pi, min_el=0, max_el=pi
     ))
 
@@ -69,20 +69,20 @@ if __name__ == "__main__":
     #             mesh=o3d.io.read_triangle_mesh("assets/lumpy_8x8.ply")
     #         ))
     #
-    # if args.cubes:
-    #     cube_xx, cube_yy = bottom_xx, bottom_yy
-    #     cube_coords = np.stack(np.meshgrid(cube_xx, cube_yy), axis=2)
-    #
-    #     cube_coords += np.random.normal(loc=0, scale=0.6, size=cube_coords.shape)
-    #     cube_coords = cube_coords.reshape(-1, 2)
-    #
-    #     for x, y in cube_coords:
-    #         surfaces.append(Surface(
-    #             id=f'cube-{x}-{y}',
-    #             pose=SE3.Rt(SO3(), np.array([x, y, -1.5])),
-    #             material=sand_material,
-    #             mesh=o3d.io.read_triangle_mesh("assets/cube_10cm.ply")
-    #         ))
+    if args.cubes:
+        cube_xx, cube_yy = bottom_xx, bottom_yy
+        cube_coords = np.stack(np.meshgrid(cube_xx, cube_yy), axis=2)
+
+        cube_coords += np.random.normal(loc=0, scale=0.6, size=cube_coords.shape)
+        cube_coords = cube_coords.reshape(-1, 2)
+
+        for x, y in cube_coords:
+            surfaces.append(Surface(
+                id=f'cube-{x}-{y}',
+                pose=SE3.Rt(SO3(), np.array([x, y, 0])),
+                material=sand_material,
+                mesh=o3d.io.read_triangle_mesh("assets/cube_10cm.ply")
+            ))
 
     scene = Scene(
         sources=sources,
@@ -94,9 +94,9 @@ if __name__ == "__main__":
         keyposes=[
             SE3.Trans(-0.5, -0.5, 0),
             SE3.Trans(0.0, -0.5, 0),
-            SE3.Trans(0.5, 0.5, 0),
-            SE3.Trans(-0.5, 0.5, 0),
-            SE3.Trans(-0.5, -0.5, 0)
+            # SE3.Trans(0.5, 0.5, 0),
+            # SE3.Trans(-0.5, 0.5, 0),
+            # SE3.Trans(-0.5, -0.5, 0)
         ],
         max_velocity=0.1,
         acceleration=100,